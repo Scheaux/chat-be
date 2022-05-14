d028248182e95b438cbfbf6779cec78e291591c3
Scheaux <125101@inbox.ru>
2022-05-14 12:00:23 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const Koa = require('koa');
+const cors = require('@koa/cors');
+const koaBody = require('koa-body');
+const Router = require('koa-router');
+const WS = require('ws');
+
+const router = new Router();
+const app = new Koa();
+const port = process.env.PORT || 1733;
+const server = http.createServer(app.callback());
+const wsServer = new WS.Server({ server });
+
+let messages = [];
+
+app.use(cors());
+app.use(koaBody());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+router.get('/messages', async (ctx, next) => {
+  ctx.response.body = messages;
+  await next();
+});
+
+wsServer.on('connection', (ws) => {
+  const clients = Array.from(wsServer.clients).filter((o) => o.readyState === WS.OPEN);
+
+  ws.on('message', (msg) => {
+    messages.push(JSON.parse(msg));
+    clients.forEach((x) => x.send(msg.toString()));
+  });
+});
+
+server.listen(port);
+// app.listen(port);
+// console.log(`listening on port: ${port}`);
